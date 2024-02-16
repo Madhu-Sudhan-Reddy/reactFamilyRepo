f90f2f49ec674a5cfb81ed3552e6a593c269117b
Madhu Sudhan Reddy Aileni <amsreddy1510@gmail.com>
2024-02-16 15:21:17 -0800

    reactFamily commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import logo from './Soccerball.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [index,setIndex]=useState(0);
+  const matter=[["Chiarnjeevi","Ram Charan","Klin Kaara"],["Allu Ramalingaiah","Allu Aravind","Allu Arjun", "Allu Ayaan"],["Sr. NTR","Hari Krishna","Jr. NTR","Abhay", "Bhargav"]];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" /> 
+      <div>
+       <h1 className="heading">Family Tree</h1>
+       <p className="description">This is the family tree of three popular families.</p>
+      </div>
+        </header>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        
+        <div id="tabs">
+        <menu>
+          <button className={index===0?"active":""} onClick={()=>setIndex(0)}>
+            Mega
+          </button>
+          <button className={index===1?"active":""} onClick={()=>setIndex(1)}>
+            Allu
+          </button>
+          <button className={index===2?"active":""} onClick={()=>setIndex(2)}>
+            NTR
+          </button>
+          </menu>
+       
+        <div id="tab-content">
+        <ul>
+          {matter[index].map((item)=>(
+            <li key={item}>{item}</li>
+          )
+
+          )
+          }
+        </ul>
+        </div>
+        </div>
+      
     </div>
   );
 }
